e0e33ff591d010d0d608546fb9e57ce55019323a
Ali-Mhrez <alisdmhrez@gmail.com>
2022-03-16 12:02:53 -0700

    FIX: fixing fchecks bidder name length constraint

---
+++ database/migrations/2022_03_16_185829_update_fhecks_table_bidder_name_field.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateFhecksTableBidderNameField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fchecks', function (Blueprint $table) {
+            $table->string('bidder_name')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
